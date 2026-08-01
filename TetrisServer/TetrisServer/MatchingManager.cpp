2d80e6e0b065146124925064d02b8513c00e3698
HyunSik <rotmzmfh@gmail.com>
2026-08-01 20:51:52 +0900

    feat: 매칭 구현
    - 간단하게 매칭을 돌리는 인원 아무나 매칭되도록 구현
    - 별도 스레드에서 매칭 인원이 있을때만 깨어나는 방식

--- TetrisServer/TetrisServer/MatchingManager.cpp
+++ TetrisServer/TetrisServer/MatchingManager.cpp
@@ -1,3 +1,39 @@
 #include "Includes.h"
 #include "UserSession.h"
 #include "MatchingManager.h"
+
+unsigned int WINAPI MatchingManager::MatchingThread(LPVOID arg)
+{
+	// 30fps
+	const DWORD dwTick = 33;
+	MatchingManager* pMatchManager = (MatchingManager*)arg;
+
+	while (1)
+	{
+		bool bLoop = false;
+		AcquireSRWLockShared(&pMatchManager->_QueueLock);
+		bLoop = !pMatchManager->_MatchingQ.empty();
+		ReleaseSRWLockShared(&pMatchManager->_QueueLock);
+
+		DWORD waitMS = bLoop ? dwTick : INFINITE;
+		WaitForSingleObject(pMatchManager->_hMatchEvent, waitMS);
+
+		pMatchManager->MatchFind();
+	}
+}
+
+void MatchingManager::MatchFind()
+{
+	if (_MatchingQ.size() < 2)
+		return;
+
+	AcquireSRWLockExclusive(&_QueueLock);
+	st_USER* pUser1 = _MatchingQ.front();
+	_MatchingQ.pop();
+
+	st_USER* pUser2 = _MatchingQ.front();
+	_MatchingQ.pop();
+	ReleaseSRWLockExclusive(&_QueueLock);
+
+	Notify(pUser1, pUser2);
+}
--- TetrisServer/TetrisServer/MatchingManager.h
+++ TetrisServer/TetrisServer/MatchingManager.h
@@ -7,12 +7,20 @@ public:
 
 	void InitMatchingManager(LPVOID context, OnMatchingCallback callback)
 	{
+		_Context = context;
+		_OnMatchFound = callback;
 
+		_hMatchEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+		_hMatchingThreadHandle = (HANDLE)_beginthreadex(NULL, 0, MatchingThread, this, 0, &_hMatchingThreadID);
 	}
 
 	void Enqueue(st_USER* pUser)
 	{
+		AcquireSRWLockExclusive(&_QueueLock);
+		_MatchingQ.push(pUser);
+		ReleaseSRWLockExclusive(&_QueueLock);
 
+		SetEvent(_hMatchEvent);
 	}
 private:
 	void Notify(st_USER* pUser1, st_USER* pUser2)
@@ -21,11 +29,18 @@ private:
 			_OnMatchFound(_Context, pUser1, pUser2);
 	}
 
+	void MatchFind();
+
 	OnMatchingCallback _OnMatchFound = nullptr;
 	void* _Context = nullptr;
 
 	// 성공하면 콜백으로 알려주기
 	queue<st_USER*> _MatchingQ;
+	SRWLOCK _QueueLock;
+
+	HANDLE _hMatchEvent = NULL;
+	HANDLE _hMatchingThreadHandle = NULL;
+	unsigned int _hMatchingThreadID;
 
 	static unsigned int WINAPI MatchingThread(LPVOID arg);
 };
